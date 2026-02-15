35942bb293dd8bc22f091bb77e2ac9988b952fae
clemPera <clement.pera@gmail.com>
2026-02-15 12:20:52 +0100

    fix/client: version number

--- notto-client/src/components/AccountMenu.tsx
+++ notto-client/src/components/AccountMenu.tsx
@@ -160,8 +160,6 @@ export default function AccountMenu() {
 
   return (
     <div className="border-t border-slate-700 bg-slate-800/50">
-      {/* Version number */}
-      <div className="text-center">version: {versionNumber}</div>
 
       {/* Sync Status */}
       <div className="px-2 md:px-3 py-2 flex items-center gap-2 text-xs md:text-sm">
@@ -177,6 +175,9 @@ export default function AccountMenu() {
                   "Sync Error"}
         </span>
       </div>
+
+      {/* Version number */}
+      <div className="text-white text-xs px-2">version: {versionNumber}</div>
 
       {/* Workspace Menu */}
       <div className="relative" ref={accountMenuRef}>
